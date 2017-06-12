b4ea648d1bb360915c87ad1e953529a15ed2407e
rmbozhko <yamakasi.romka@yahoo.fr>
2017-06-12 13:41:18 +0300

    Verbesserung des choosing
    Choosing the better option problem using Manhattan algo.

--- ft_answers_creation.c
+++ ft_answers_creation.c
@@ -1,25 +1,32 @@
 #include "filler.h"
 
 // Can we use bresenheim somehow to calc the answer, which has min length to opp_sign?
+#define HAS_DOT_AROUND (bot->field[y][x - 1] == '.' || bot->field[y][x + 1] == '.'
+|| bot->field[y - 1][x] == '.' || bot->field[y + 1][x] == '.') // TAKE Care about seg faults x + 1 || y - 1 can cause.
+#define ABS(x) (x < 0) ? -x : x // https://stackoverflow.com/questions/2025372/c-macro-question-x-vs-x
+	
 static int			ft_calc_square(t_bot *bot)
 {
-	int		counter;
+	int		max_distance;
+	int		result;
 	int		x;
 	int		y;
 
-	y = bot->answer_y - 1;
-	counter = -1;
+	y = -1;
+	max_distance = bot->field_width + bot->field_height;
 	while (bot->field[++y])
 	{
-		x = bot->answer_x;
+		x = -1;
 		while (bot->field[y][++x])
 		{
-			++counter;
-			if (bot->field[y][x] == bot->opp_sign)
-				break ;	
+			if (bot->field[y][x] == bot->opp_sign && HAS_DOT_AROUND)
+			{
+				result = ABS(x - bot->answer_x) + ABS(y - bot->answer_y);
+				max_distance = (result < max_distance) ? result : max_distance;
+			}
 		}
 	}
-	return (counter);
+	return (max_distance);
 }
 
 void		ft_create_solution(t_bot *bot, int fd)
